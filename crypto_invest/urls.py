4de4d9cfe66eed853de45647b4678877dbf5fcc3
Dekatron322 <muritalaibrahim097@gmail.com>
2021-09-23 05:07:21 -0700

    final stages

---
+++ crypto_invest/urls.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    path("", include("main.urls")),
+    path("", include("admin_app.urls")),
+    path("", include("app_user.urls")),
+    path("", include("wallet.urls")),
+
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
